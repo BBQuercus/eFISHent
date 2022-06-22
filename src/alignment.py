5eac51452612c825d96fb06b8acf113226a8f207
Bastian Eichenberger <bastian@eichenbergers.ch>
2022-06-22 17:16:07 +0200

    Filter gene of interest and save fpkm table

--- src/alignment.py
+++ src/alignment.py
@@ -59,26 +59,33 @@ class AlignProbeCandidates(luigi.Task):
     )
 
     def _requires(self):
+        """Hidden / not directly accessed requirements."""
         tasks = [BuildBowtieIndex()]
         if self.is_blast_required:
             tasks.append(BuildBlastDatabase())
         return luigi.task.flatten(
-            [
-                *tasks,
-                super(AlignProbeCandidates, self)._requires(),
-            ]
+            [*tasks, super(AlignProbeCandidates, self)._requires()]
         )
 
     def requires(self):
         tasks = {"probes": BasicFiltering()}
-        # if self.is_blast_required:
-        #     tasks.append(PrepareSequence())
+        if self.is_blast_required:
+            tasks["blastseq"] = PrepareSequence()
         return tasks
 
     def output(self):
-        return luigi.LocalTarget(
-            os.path.join(util.get_output_dir(), f"{util.get_gene_name()}_aligned.fasta")
-        )
+        tasks = {
+            "fasta": luigi.LocalTarget(
+                os.path.join(
+                    util.get_output_dir(), f"{util.get_gene_name()}_aligned.fasta"
+                )
+            )
+        }
+        if self.is_blast_required:
+            tasks["table"] = luigi.LocalTarget(
+                os.path.join(util.get_output_dir(), f"{util.get_gene_name()}_fpkm.csv")
+            )
+        return tasks
 
     @staticmethod
     def align_probes(fname_fasta: str, fname_sam: str) -> None:
@@ -119,46 +126,75 @@ class AlignProbeCandidates(luigi.Task):
             [row.split("\t") for row in filtered_sam.split("\n")],
             columns=constants.SAMFILE_COLUMNS,
         )
-        # order = list(df[df["qname"] != ""].groupby("qname").size().sort_values().keys())
-        # sequences_ordered = []
-        # for name in order:
-        #     for seq in sequences:
-        #         if seq.id == name:
-        #             sequences_ordered.append(seq)
         return df
 
     def filter_gene_of_interest(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Filter probes based on gene of interest."""
-        fname = os.path.join(
-            util.get_output_dir(), f"{util.get_gene_name()}_blast.fasta"
-        )
+        """Filter FPKM table to exclude the gene of interest."""
+        # Filter using provided EnsembleID directly
+        if SequenceConfig().ensemble_id:
+            self.logger.debug(
+                f"Filtering directly using EmsembleID {SequenceConfig().ensemble_id}"
+            )
+            return df[df["clean_id"] != SequenceConfig().ensemble_id]
+
+        # Filter using blast
+        fname = os.path.join(util.get_output_dir(), f"{util.get_gene_name()}_blast.txt")
         os.system(
             f"blastn\
                 -db {util.get_genome_name()}\
-                -query {self.input()[1].path}\
+                -query {self.input()['blastseq'].path}\
                 -task megablast\
                 -outfmt 6\
                 -num_threads {GeneralConfig().threads} > {fname}"
         )
-        df_blast = pd.read_csv("./test.txt", sep="\t", names=constants.BLAST_COLUMNS)
-        df_blast = df_blast[df_blast["evalue"] == 0]
-        # TODO add filter for gene of interest
+        df_blast = pd.read_csv(fname, sep="\t", names=constants.BLAST_COLUMNS)
+        df_blast = df_blast[
+            (df_blast["pident"] >= 98)
+            & (df_blast["evalue"] <= 1e-8)
+            & (df_blast["sseqid"] == df_blast.loc[0, "sseqid"])
+        ]
+
+        # Pad blast start/end results in case not everything was found/isoforms/etc.
+        # Set to an arbitrary maximum value of 100 to not include other genes
+        for buffer in range(0, 110, 10):
+            gene_ids = []
+            for _, row in df_blast.iterrows():
+                gene_ids.extend(
+                    df[
+                        (df["seqname"] == row["sseqid"])
+                        & (df["start"] >= row["sstart"] - buffer)
+                        & (df["end"] <= row["send"] + buffer)
+                    ]["clean_id"].values
+                )
+            if not gene_ids:
+                self.logger.debug(f"No genes found using a buffer of {buffer}")
+                continue
+
+            most_frequent_gene_id = max(set(gene_ids), key=gene_ids.count)
+            self.logger.debug(
+                f"Filtering out most frequent gene {most_frequent_gene_id}"
+            )
+            df = df[df["clean_id"] != most_frequent_gene_id].reset_index(drop=True)
+            break
+
         return df
 
-    def filter_fpkm(self, df: pd.DataFrame) -> pd.DataFrame:
-        """Filter candidates based on FPKM."""
-        # Read gtf file
-        df_gtf = gtfparse.read_gtf(GeneralConfig().reference_annotation)
-
-        # Read count table
+    @staticmethod
+    def read_count_table() -> pd.DataFrame:
+        """Read and verify a RNAseq FPRKM count table."""
         df_counts = pd.read_csv(ProbeConfig().encode_count_table, sep="\t")
         counts = df_counts[df_counts["gene_id"].str.contains("ENSG")].copy()
         counts["clean_id"] = counts["gene_id"].apply(lambda x: x.split(".")[0])
+        return counts[constants.COUNTS_COLUMNS]
 
+    def get_maximum_fpkm(
+        self, df_sam: pd.DataFrame, df_counts: pd.DataFrame, df_gtf: pd.DataFrame
+    ) -> pd.DataFrame:
+        """Filter candidates based on FPKM."""
         # Merge gtf and count table to get FPKM at each genomic location
         df_fpkm = pd.merge(
-            counts[constants.COUNTS_COLUMNS],
-            df_gtf[constants.GTF_COLUMNS],
+            df_counts,
+            df_gtf,
             how="left",
             left_on="clean_id",
             right_on="gene_id",
@@ -167,49 +203,72 @@ class AlignProbeCandidates(luigi.Task):
         if self.is_blast_required:
             df_fpkm = self.filter_gene_of_interest(df_fpkm)
 
-        # Match FPKM to the probe candidates' genomic locations
+        # Match FPKM to the probe candidates' off-target genomic locations
+        # Loop over chromosome/rname to keep loci separated
         dfs = []
-        for sequence, df_sequence in tqdm(df.groupby("rname")):
-            low = df_fpkm[df_fpkm["seqname"] == sequence]["start"].astype(int).values
-            high = df_fpkm[df_fpkm["seqname"] == sequence]["end"].astype(int).values
-            vals = df_sequence["pos"].astype(int).values
-            i, j = np.where((vals[:, None] >= low) & (vals[:, None] <= high))
-            dfs.append(
-                pd.concat(
-                    [
-                        df_sequence.reset_index(drop=True)
-                        .iloc[i]
-                        .reset_index(drop=True),
-                        df_fpkm[df_fpkm["seqname"] == sequence]
-                        .reset_index(drop=True)
-                        .iloc[j]
-                        .reset_index(drop=True),
-                    ],
-                    axis=1,
+        for sequence, df_sequence in tqdm(df_sam.groupby("rname")):
+            df_fpkm_sequence = df_fpkm[df_fpkm["seqname"] == sequence]
+            gene_start = df_fpkm_sequence["start"].astype(int).values
+            gene_end = df_fpkm_sequence["end"].astype(int).values
+            align_start = df_sequence["pos"].astype(int).values
+            align_end = align_start + df_sequence["seq"].apply(len).values
+
+            # Create n*m matrix of which alignments(n) match up with which annotations(m)
+            i, j = np.where(
+                (
+                    (align_start[:, None] >= gene_start)
+                    & (align_start[:, None] <= gene_end)
+                )
+                | (
+                    (align_end[:, None] >= gene_start)
+                    & (align_end[:, None] <= gene_end)
                 )
             )
+
+            # Join columns to merge alignment information with FPKM values
+            df_sequence = pd.concat(
+                [
+                    df_sequence.reset_index(drop=True).iloc[i].reset_index(drop=True),
+                    df_fpkm_sequence.reset_index(drop=True)
+                    .iloc[j]
+                    .reset_index(drop=True),
+                ],
+                axis=1,
+            )
+            dfs.append(df_sequence)
         dfs = pd.concat(dfs, ignore_index=True)
 
-        # Filter out probes with FPKM > max_fpkm
+        # Get the highest FPKM per alignment/candidate
         df_max_fpkm = dfs.groupby("qname", as_index=False)["FPKM"].max()
-        candidates = df_max_fpkm[
-            df_max_fpkm["FPKM"] > ProbeConfig().max_off_target_fpkm
+        return df_max_fpkm
+
+    def read_gtf_file(self):
+        return gtfparse.read_gtf(GeneralConfig().reference_annotation)[
+            constants.GTF_COLUMNS
         ]
-        return candidates
 
     def run(self):
         fname_fasta = self.input()["probes"].path
         fname_sam = os.path.splitext(fname_fasta)[0] + ".sam"
         self.align_probes(fname_fasta, fname_sam)
-        df = self.filter_unique_probes(fname_sam)
+        df_sam = self.filter_unique_probes(fname_sam)
 
-        if ProbeConfig().encode_count_table != "None":
-            df = self.filter_fpkm(df)
+        if ProbeConfig().encode_count_table:
+            df_gtf = self.read_gtf_file()
+            df_counts = self.read_count_table()
+            df_max_fpkm = self.get_maximum_fpkm(df_sam, df_counts, df_gtf)
+            df_max_fpkm.to_csv(self.output()["table"].path, index=False)
+            candidates = df_max_fpkm[
+                df_max_fpkm["FPKM"] > ProbeConfig().max_off_target_fpkm
+            ]
 
         # Filter candidates found in samfile
         sequences = list(Bio.SeqIO.parse(fname_fasta, "fasta"))
-        candidates = [seq for seq in tqdm(sequences) if seq.id in df["qname"].values]
+        # TODO error overwriting candidates
+        candidates = [
+            seq for seq in tqdm(sequences) if seq.id in df_sam["qname"].values
+        ]
         util.log_and_check_candidates(
             self.logger, "AlignProbeCandidates", len(candidates), len(sequences)
         )
-        Bio.SeqIO.write(candidates, self.output().path, "fasta")
+        Bio.SeqIO.write(candidates, self.output()["fasta"].path, "fasta")
