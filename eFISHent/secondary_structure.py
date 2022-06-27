d1cb856afef3436d9173296e6140f0d4f32afccd
BBQuercus <bastian@eichenbergers.ch>
2022-06-27 12:14:02 +0200

    Add data_tables; Very hacky Fold way for MacOS

--- eFISHent/secondary_structure.py
+++ eFISHent/secondary_structure.py
@@ -2,11 +2,13 @@
 Predict secondary structure of probes and filter them based on deltaG.
 """
 
+from pathlib import Path
 import logging
 import multiprocessing
 import os
 import re
 import subprocess
+import sys
 import tempfile
 
 import Bio.SeqIO
@@ -21,20 +23,28 @@ from . import util
 
 def get_free_energy(sequence: Bio.SeqRecord) -> float:
     """Return the predicted free energy of the sequence."""
-    os.environ["DATAPATH"] = "./data_tables/"
+    file_path = Path(__file__).resolve().parent.as_posix()
+    data_table = os.path.join(file_path, "data_tables/")
+    if sys.platform == "linux" or sys.platform == "linux2":
+        fold_path = "./Fold"
+    if sys.platform == "darwin":
+        fold_path = os.path.join(file_path, "Fold_osx")
+
+    os.environ["DATAPATH"] = data_table
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         fname = os.path.join(tmpdirname, "input.fasta")
         Bio.SeqIO.write(sequence, fname, "fasta")
-        args_fold = ["Fold", fname, "-", "--bracket", "--MFE"]
+        args_fold = [fold_path, fname, "-", "--bracket", "--MFE"]
         sec = subprocess.check_output(args_fold, stderr=subprocess.STDOUT).decode()
 
     # Format with secondary structure:
     # >ENERGY = -deltaG NAME DESCRIPTION\nSEQENCE\nDOT-BRACKET
-    if search := re.search(r">ENERGY = (-\d+\.\d)\s\s([\w-]+)", sec):
-        energy = float(search.groups()[0])
     # Format without secondary structure:
     # >NAME DESCRIPTION\nSEQUENCE\n...
+    search = re.search(r">ENERGY = (-\d+\.\d)\s\s([\w-]+)", sec)
+    if search is not None:
+        energy = float(search.groups()[0])
     else:
         energy = 0.0
     return energy
@@ -63,7 +73,7 @@ class SecondaryStructureFiltering(luigi.Task):
         candidates = [
             sequence
             for sequence, energy in zip(sequences, free_energies)
-            if energy >= ProbeConfig().max_deltaG
+            if energy >= ProbeConfig().max_deltag
         ]
 
         util.log_and_check_candidates(
