a3d1cbd5bf7fd361aaa9834dbcc7d9406ba0b3ae
Bastian Eichenberger <bastian@eichenbergers.ch>
2023-03-15 21:26:36 +0100

    Clean up bash script

--- actions/conda/entrypoint.sh
+++ actions/conda/entrypoint.sh
@@ -4,8 +4,8 @@ set -ex
 set -o pipefail
 
 go_to_build_dir() {
-    if [ ! -z $INPUT_SUBDIR ]; then
-        cd $INPUT_SUBDIR
+    if [ ! -z "${INPUT_SUBDIR}" ]; then
+        cd "${INPUT_SUBDIR}"
     fi
 }
 
@@ -24,37 +24,41 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
-    # Define a function to add the chanels
+    # Define a function to add the channels
     function join_by {
-    local d=${1-} f=${2-}
-    if shift 2; then
-        printf %s "$f" "${@/#/$d}"
-    fi
+        local d=${1-} f=${2-}
+        if shift 2; then
+            printf %s "$f" "${@/#/$d}"
+        fi
     }
-    
+
     # Build for Linux
-    build_cmd="conda build -c $(echo $(join_by ' -c ' $INPUT_CHANNELS)) --output-folder . ."
-    eval build_cmd
+    build_cmd="conda build -c $(echo $(join_by ' -c ' "${INPUT_CHANNELS[@]}")) --output-folder . ."
+    eval "$build_cmd"
 
     # Convert to other platforms: OSX, WIN
-    if [[ $INPUT_PLATFORMS == *"osx"* ]]; then
-    conda convert -p osx-64 linux-64/*.tar.bz2
+    if [[ "${INPUT_PLATFORMS}" == *"osx"* ]]; then
+        conda convert -p osx-64 linux-64/*.tar.bz2
     fi
-    if [[ $INPUT_PLATFORMS == *"win"* ]]; then
-    conda convert -p win-64 linux-64/*.tar.bz2
+    if [[ "${INPUT_PLATFORMS}" == *"win"* ]]; then
+        conda convert -p win-64 linux-64/*.tar.bz2
     fi
 }
 
 upload_package(){
-    export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    if [[ $INPUT_PLATFORMS == *"osx"* ]]; then
-    anaconda upload --label main osx-64/*.tar.bz2
+    if [ -z "$ANACONDA_API_TOKEN" ]; then
+        echo "ANACONDA_API_TOKEN not set"
+        exit 1
     fi
-    if [[ $INPUT_PLATFORMS == *"linux"* ]]; then
-    anaconda upload --label main linux-64/*.tar.bz2
+
+    if [[ "${INPUT_PLATFORMS}" == *"osx"* ]]; then
+        anaconda upload --label main osx-64/*.tar.bz2 || exit 1
     fi
-    if [[ $INPUT_PLATFORMS == *"win"* ]]; then
-    anaconda upload --label main win-64/*.tar.bz2
+    if [[ "${INPUT_PLATFORMS}" == *"linux"* ]]; then
+        anaconda upload --label main linux-64/*.tar.bz2 || exit 1
+    fi
+    if [[ "${INPUT_PLATFORMS}" == *"win"* ]]; then
+        anaconda upload --label main win-64/*.tar.bz2 || exit 1
     fi
 }
 
